59244db2c546a4ffbb64e4eb7b4dd92f636f4d40
Matheus Sena <senamatheusjob@gmail.com>
2023-04-21 20:08:57 -0300

    Unit test

--- src/lists/lists.service.spec.ts
+++ src/lists/lists.service.spec.ts
@@ -1,29 +1,22 @@
+import { of } from 'rxjs';
 import { List } from './entities/list.entity';
+import { ListGatewayInMemory } from './gateways/list-gateway-in-memory';
 import { ListsService } from './lists.service';
 
 const mockHttpService = {
-  post: jest.fn(),
+  post: jest.fn().mockReturnValue(of(null)),
 };
 
 describe('ListsService', () => {
   let service: ListsService;
-
+  let listGateway: ListGatewayInMemory;
   beforeEach(() => {
-    service = new ListsService(mockList as any, mockHttpService as any);
+    listGateway = new ListGatewayInMemory();
+    service = new ListsService(listGateway, mockHttpService as any);
   });
 
   it('Deve criar uma list', async () => {
     const list = await service.create({ name: 'my list' });
-    console.log(list);
+    expect(listGateway.items).toEqual([list]);
   });
-  // let service: ListsService;
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     providers: [ListsService],
-  //   }).compile();
-  //   service = module.get<ListsService>(ListsService);
-  // });
-  // it('should be defined', () => {
-  //   expect(service).toBeDefined();
-  // });
 });
